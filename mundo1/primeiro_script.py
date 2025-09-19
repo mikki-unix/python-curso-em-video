a3cb75c6162bb5dce5fda12118d6c0bccea1886f
mikki-unix <mikki.anjos@sptech.school>
2025-09-19 15:08:22 -0300

    Início Mundo1
    Nas fases 3 e 4, foram feitas manipulações básicas de strings e uso da função `print()` e `input()`.
    
    Foram propostos desafios, mas o que foquei foi de exibir uma soma, o qual a pegadinha era que `ìnput()` sempre retorna strings, e então deveria se fazer uma conversão para que a soma funcionasse corretamente. O professor não disse qual a função de conversão, mas chutando descobri a função int() que acabou resolvendo o que eu precisava.

---
+++ mundo1/primeiro_script.py
@@ -0,0 +1,5 @@
+nome = input('Qual teu nome? ')
+idade = input('Quantos anos você tem? ')
+peso = input('Quantos quilos você pesa? ')
+
+print("\n",nome,"tem",idade,"anos","e pesa",peso,"kg")
---
+++ mundo1/soma.py
@@ -0,0 +1,5 @@
+# input() retorna uma string. por isso tem q converter com int()
+numero1 = int(input("Número 1 = "))
+numero2 = int(input("Número 2 = "))
+
+print("\nO resultado é", (numero1+numero2))
