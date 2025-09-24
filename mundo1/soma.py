828dbc5b5f9074454e0516ff9899a62506d760e7
mikki-unix <mikki.anjos@sptech.school>
2025-09-24 11:29:00 -0300

    reorganização

--- mundo1/soma.py
+++ mundo1/soma.py
@@ -1,5 +1,7 @@
 # input() retorna uma string. por isso tem q converter com int()
-numero1 = int(input("Número 1 = "))
-numero2 = int(input("Número 2 = "))
+n1 = int(input("Número 1 = "))
+n2 = int(input("Número 2 = "))
 
-print("\nO resultado é", (numero1+numero2))
+# print('\nn1 é um {} e n2 é um {}'.format(type(n1), type(n2)))
+
+print('\nA soma entre {} e {} é {}'.format(n1, n2, n1+n2))
