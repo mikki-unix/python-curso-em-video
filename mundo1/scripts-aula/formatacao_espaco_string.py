d2be116bf961c1f59d3d01f65f396e5f245d8590
mikki-unix <mikki.anjos@sptech.school>
2025-09-26 14:24:47 -0300

    Mais uma formatação e comentários

--- mundo1/scripts-aula/formatacao_espaco_string.py
+++ mundo1/scripts-aula/formatacao_espaco_string.py
@@ -1,4 +1,9 @@
 palavra = input('Digite algo: ')
 
+# : é o início da especificação da formatação
+# ~ foi o carácter que escolhi para preencher o espaço
+# <^> define a direção em que a string deve ficar
+# o número no final define o espaço total do campo em que a string será exibida
+print('\nÀ esquerda: \n{:~<20}'.format(palavra))
 print('\nCentralizado: \n{:~^20}'.format(palavra))
 print('\nÀ direita: \n{:~>20}'.format(palavra))
