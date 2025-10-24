7d37a7f3aa54e003ce177c9496d7d97a17459ac5
mikki-unix <mikki.anjos@sptech.school>
2025-10-24 15:53:26 -0300

    edit ex039 - adicionado ano de alistamento

--- mundo2/ex039.py
+++ mundo2/ex039.py
@@ -1,12 +1,22 @@
 from datetime import date
 
-nasc = int(input('Digite o ano de nascimento de um jovem, para saber se ele deve se alistar:\n'))
-
-idade = date.today().year - nasc
+nasc = int(input('Digite o ano de nascimento de um jovem,\npara saber se ele deve se alistar:\n'))
+atual = date.today().year
+idade = atual - nasc
 
 if idade < 18:
-  print('\nEle tem {} anos de idade, faltam {} anos para precisar se alistar'.format(idade, 18 - idade))
+  diff = 18 - idade
+  print(
+    '\nEle tem {} anos de idade. Faltam {} anos para precisar se alistar'
+    '\nSeu ano de alistamento será em {}'
+    .format(idade, diff, atual + diff)
+  )
 elif idade == 18:
-  print('\nJá possui 18 anos, portanto deve se alistar')
+  print('\nJá possui 18 anos, portanto deve se alistar este ano')
 else:
-  print('\nEle tem {} anos. Se ainda não se alistou, está {} anos atrasado'.format(idade, idade - 18))
+  diff = idade - 18
+  print(
+    '\nEle tem {} anos. Se ainda não se alistou, está {} anos atrasado'
+    '\nSeu ano de alistamento foi em {}'
+    .format(idade, diff, atual - diff)
+  )
